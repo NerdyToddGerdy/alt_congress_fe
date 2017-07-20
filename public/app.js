cefb47035f81c93930883059305a0dfc772141da
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-07-20 16:35:55 -0400

    add POST

--- public/app.js
+++ public/app.js
@@ -3,14 +3,29 @@ console.log('test');
 console.log('====================================');
 
 var app = angular.module('altCong', []);
+myURL = 'https://alt-congress-api.herokuapp.com'
 
 app.controller('mainController', ['$http', function($http) {
     // this.message = "controller works";
     this.users = [];
+    this.formdata = {};
+
+    this.processForm = function() {
+        console.log('Formdata: ', this.formdata);
+        $http({
+            method: 'POST',
+            url: myURL + '/users',
+            data: this.formdata
+        }).then(function(result){
+            console.log('====================================');
+            console.log('Data from server: ', result);
+            console.log('====================================');
+        })
+    }
 
     $http({
         method: 'GET',
-        url: 'http://localhost:3000/users'
+        url: myURL + '/users'
     }).then(function(response){
         // console.log('====================================');
         console.log(response);
